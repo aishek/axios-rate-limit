d224a76994ba40a8f2cdb81570c13b6596a7aa02
Benny Neugebauer <bn@bennyn.de>
2020-02-28 07:35:21 +0100

    Add optional maxRPS config value

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,7 +4,7 @@ interface RateLimitedAxiosInstance extends AxiosInstance {}
 
 declare function axiosRateLimit(
     axiosInstance: AxiosInstance,
-    options: { maxRequests: number, perMilliseconds: number },
+    options: { maxRequests: number, perMilliseconds: number, maxRPS?: number },
 ): RateLimitedAxiosInstance;
 
 export = axiosRateLimit;
